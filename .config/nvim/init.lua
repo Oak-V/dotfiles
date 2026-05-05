5287568f5e2acff76ce14d5ba57544c2f7098d65
Vinícius Carvalho <vinicius@motim.io>
2026-05-05 11:14:44 -0300

    chore: move spoons to dev folder

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,25 @@
 vim.opt.nu = true
 vim.opt.rnu = true
 
+vim.o.autocomplete = true
+vim.o.pummaxwidth = 40
+vim.o.completeopt = 'menu,menuone,noselect'
+
+vim.lsp.config['lua_ls'] = {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.git' },
+  settings = { Lua = { diagnostics = { globals = { "vim" } } } }
+}
+vim.lsp.enable('lua_ls')
+
+vim.lsp.config['rust_analyzer'] = {
+    cmd = { 'rust-analyzer' },
+    filetypes = { 'rust' },
+    root_markers = { 'Cargo.toml' },
+}
+vim.lsp.enable('rust_analyzer')
+
 vim.opt.mouse = ""
 
 vim.g.clipboard = {
@@ -18,3 +37,21 @@ vim.g.clipboard = {
 
 vim.opt.clipboard = "unnamedplus"
 
+local sev = vim.diagnostic.severity
+
+vim.diagnostic.config({
+  severity_sort = true,
+  update_in_insert = false,
+  float = {
+    border = 'rounded',
+    source = true,
+  },
+  signs = {
+    text = {
+      [sev.ERROR] = 'E',
+      [sev.WARN]  = 'W',
+      [sev.INFO]  = 'I',
+      [sev.HINT]  = 'H',
+    },
+  },
+})
--- .config/zsh/.zlogin
+++ .config/zsh/.zlogin
@@ -2,5 +2,6 @@ source "$ZDOTDIR/keyboard.sh"
 
 autoload -U +X bashcompinit && bashcompinit
 autoload -Uz compinit && compinit
+eval "$(tree-sitter complete --shell zsh)"
 
 source <(fzf --zsh)
